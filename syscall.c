9e520697c59c16b8c3c31edd5b09851b4889b21f
ClayWW <claywolfang@gmail.com>
2022-10-24 16:34:31 -0400

    Changed how the system call worked

--- syscall.c
+++ syscall.c
@@ -7,7 +7,6 @@
 #include "x86.h"
 #include "syscall.h"
 
-extern int sys_crsp(void);
 // User code makes a system call with INT T_SYSCALL.
 // System call number in %eax.
 // Arguments on the stack, from the user call to the C
@@ -104,6 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_crsp(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
