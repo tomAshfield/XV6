4108f7925666aec882e03697fac8c97aaa56b3c6
ClayWW <claywolfang@gmail.com>
2022-10-24 16:37:41 -0400

    Changed return type again

--- syscall.c
+++ syscall.c
@@ -103,7 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int sys_crsp(void);
+extern void sys_crsp(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
