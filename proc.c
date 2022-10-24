9a0dc930982565e23b5de52e828cbe90a80d6edc
ClayWW <claywolfang@gmail.com>
2022-10-24 15:03:40 -0400

    Part 1 printf line added to proc.c

--- proc.c
+++ proc.c
@@ -342,6 +342,7 @@ scheduler(void)
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
+      printf("process [%s:%d] is running", p->name, p->pid);
 
       swtch(&(c->scheduler), p->context);
       switchkvm();
