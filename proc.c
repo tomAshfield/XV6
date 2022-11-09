10307f480f89d4a0684aaf857c83df2b03853938
ClayWW <claywolfang@gmail.com>
2022-11-09 13:38:21 -0500

    removed an if

--- proc.c
+++ proc.c
@@ -346,9 +346,11 @@ scheduler(void)
       for(q = ptable.proc; q < &ptable.proc[NPROC]; q++){   //loop through every process to look for adjustments
         //cprintf("second loop idle: %d\n", q->idle); //is 9 here
         //cprintf("Made it inside for loop two");
+        /*
         if(q->state != RUNNABLE){
           continue;
         }
+        */
         //cprintf("some are runnable");
         if(q->idle > q->totaliterations){ //if it has been idle for longer than it needs to run, move up in priority
           //cprintf("idle: %d\n", q->idle);
@@ -405,7 +407,7 @@ scheduler(void)
       int newQN = 0;
       for(r = ptable.proc; r < &ptable.proc[NPROC]; r++){
        if(r->queuenumber > newQN){
-         newQN = r->queuenumber;
+         newQN = r->queuenumber;  //max queuenumber
         }
       }
       if(newQN < qn){
